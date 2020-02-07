e58fd7daca790108c2a9b50232d4591323a500b8
Quin'darius <quinwoodz@gmail.com>
2020-02-07 15:45:28 -0500

    Git hub test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>An Unabriged Collection of Work</h1>
+    <p>Explore the site to learn more!</p>
+    <Link to="/page-2/">Git hub test!</Link>
   </Layout>
 )
 
